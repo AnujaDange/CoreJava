cc68fd0e599c00acafd0a947107fba83c993b684
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-05-20 14:36:34 +0530

    Update TernaryOp.java

--- JavaSessions/src/com/operatores/TernaryOp.java
+++ JavaSessions/src/com/operatores/TernaryOp.java
@@ -5,8 +5,8 @@ public class TernaryOp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           int no=16;
-         (no%2==0)? no+" is even": no+" is odd";
-        
+         String result=(no%2==0) ? " is even": " is odd";
+        System.out.println(result);
 	}
 
 }
