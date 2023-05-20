24f9333e3a7a27799cf36ef77b264bfac6eb1b1f
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-05-20 13:49:18 +0530

    Update A_10_GreatestofTwo.java

--- JavaSessions/src/com/assignment_Switch/A_10_GreatestofTwo.java
+++ JavaSessions/src/com/assignment_Switch/A_10_GreatestofTwo.java
@@ -6,23 +6,32 @@ public class A_10_GreatestofTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int no1,no2;
+		int n1,n2;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter two number =");
-		no1=sc.nextInt();
-		no2=sc.nextInt();
-		
-		switch(no1>=no2)
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		int res = n1>n2?1:0;
+		switch (res)
 		{
-		case 0 :
-			System.out.println(no1+" is greater than"+ no2);
-			break;
-		case 1 :
-			System.out.println(no1+" is less than"+ no2);
-			break;
-			default:
-			System.out.println("Invalid");
+			case 0:				
+				res = n1==n2?1:0;
+				switch(res)
+				{
+					case 0:						
+						System.out.printf("Maximum Number is "+n2);
+						break;					
+					case 1:						
+						System.out.printf("Both are Equals");
+						break;
+				}
+				break;
+ 
+			case 1:
+				System.out.printf("Maximum Number is "+n1);
+				break;
 		}
+
 	}
 
 }
