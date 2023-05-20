9d0d1195cd7caf254062faba65705b4a9934b604
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-05-20 13:45:45 +0530

    Update A_05_Character.java

--- JavaSessions/src/com/assignment_Loop/A_05_Character.java
+++ JavaSessions/src/com/assignment_Loop/A_05_Character.java
@@ -4,8 +4,11 @@ class A_05_Character {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          char a='a',z=z;
-          for(ichar=)
+         
+          for(char i='a' ; i<='z' ; i++)
+	  {
+		  System.out.println(i);
+	  }
 	}
 
 }
