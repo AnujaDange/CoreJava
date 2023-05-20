b6c5a3b02fa50dd36402498921b1e1fd5fab8416
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-05-20 13:43:41 +0530

    Update MulObject.java

--- JavaSessions/src/com/assignment_OOps/MulObject.java
+++ JavaSessions/src/com/assignment_OOps/MulObject.java
@@ -4,10 +4,24 @@
 package com.assignment_OOps;
 
 public class MulObject {
-
+String name;
+	int roll;
+    void set()
+    {
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("Enter name and roll no. of Student = ");
+    	 name=sc.next();
+         roll=sc.nextInt();
+    }
+    void get()
+    {
+    	System.out.println("Name = "+name);
+    	System.out.println("Roll no. ="+roll);
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MulObject s = new MulObject();
+		s.set();
+		s.get();
 	}
-
 }
