966d049fbef9ab8446aebb306ca29e3986fde1da
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2023-05-20 15:09:20 +0530

    Update StudentMethodsMain.java

--- JavaSessions/src/com/WrittenBasic_2/StudentMethodsMain.java
+++ JavaSessions/src/com/WrittenBasic_2/StudentMethodsMain.java
@@ -9,65 +9,11 @@ public class StudentMethodsMain {
 		 
 		int a=s1.hashCode();
 		int b=s2.hashCode();
-		System.out.println("Comparing obj s1 and s2= "+);
-		package examsession2;
-
-		/*
-		  1.	print 2 objects in sysout and see its hashcode is different
-		Eg Student@15db9742 and Student@2329742
-		*/
-
-		public class StudentMethods {
-
-			int id;
-			String name, email;
-
-			public StudentMethods(int id, String name, String email) {
-				this.id = id;
-				this.name = name;
-				this.email = email;
-
-			}
-
-			void setName(String name) {
-				this.name = name;
-			}
-
-			/*
-			 * public String toString() { return id + " " + name + " " + email; }
-			 */
-		}
-		package examsession2;
-
-		/*
-		  1.	print 2 objects in sysout and see its hashcode is different
-		Eg Student@15db9742 and Student@2329742
-		*/
-
-		public class StudentMethods {
-
-			int id;
-			String name, email;
-
-			public StudentMethods(int id, String name, String email) {
-				this.id = id;
-				this.name = name;
-				this.email = email;
-
-			}
-
-			void setName(String name) {
-				this.name = name;
-			}
-
-			
-		}
-	
-
-
-			
+		System.out.println("Comparing obj s1 and s2= ");
+		System.out.println(a);
+		System.out.println(b);
 		
-	
 	}
 
 }
+
